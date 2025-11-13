58127ea0f18684b148d9804c49a9aa561652295e
Liam Skirrow <skirrowliam@gmail.com>
2025-11-13 22:59:24 +0000

    Added to script

--- krypton/kry5/brute_force.py
+++ krypton/kry5/brute_force.py
@@ -11,11 +11,24 @@ import sys
 
 # this is a superset of what we want to generate
 goal_alphabet = 'ETAOINSRHDLUCMFYWGPBVKXQJZ'
-cipherKeyList = ['A', 'A', 'A']
-IDX_TOP_CHAR = 2
+cipherKeyList = ['A', 'A', 'A', 'A', 'A', 'A']
+IDX_TOP_CHAR = 5
+count = 0
+approx_complete = 0
 
 file = input('Enter source filename: ')
 pw_file = input('Enter pw filename: ')
+
+def analyse_ciphertext(text):
+    pass
+
+    return [plaintext, matching_alphabet]
+
+# apply cipherKeyList to passwd, using alphabet
+def decrypt_ciphertext_pw(passwd, cipherKeyList, alphabet):
+    print(passwd)
+    pass    
+
 
 # TODO: can be made more efficient, since we don't need to store A's or Z's since they're implicit
 # increment the 6 letter cipher key, recursively
@@ -35,19 +48,48 @@ def increment_key(ck, idx):
     
     return ck
 
+
+
+with open(pw_file, 'r') as f:
+    for line in f:
+        passwd = line.replace(' ', '')
+        print('Encrypted password: ' + line)
+
 # generate monoalphabetic-ciphers 0-5
 with open(file, 'r') as f:
     for line in f:
         line = line.replace(' ', '')
         print('Sample ciphertext:')
         print(line)
-        while(True):
-        # for i in range(0, 100):
-            print(''.join(cipherKeyList))
-            cipherKeyList = increment_key(cipherKeyList, 0)
 
+        passwd_list = []
+
+        while(True):
+            count += 1
+            if(count % 3089158 == 0):
+                approx_complete += 1
+                print(str(approx_complete) + 'pc complete...')
+            
             # we've reached the end
-            if(cipherKeyList == ['Z', 'Z', 'Z']):
+            if(cipherKeyList == ['Z', 'Z', 'Z', 'Z', 'Z', 'Z']):
                 print(''.join(cipherKeyList))
                 break
+
+            # do frequency analysis on the ciphertext
+            derived_alphabet = analyse_ciphertext(line, cipherKeyList)
+
+            # check if the derived_alphabet partially matches the goal_alphabet
+            # if so, apply the current key to the encrypted passwd and store the resultant decrypted passwd
+            [plaintext, matching_alphabet] = decrypt_ciphertext_pw(passwd, cipherKeyList, derived_alphabet)
+
+            # push to the storage list
+            if(matching_alphabet):
+                print('Found potential plaintext password: ' + plaintext + 'associated with key: ' + ''.join(cipherKeyList))
+                passwd_list.append(plaintext)
+
+            # increment the key
+            cipherKeyList = increment_key(cipherKeyList, 0)
+    
+    # TODO: don't forget to output passwd_list to a file
+
     f.close()
